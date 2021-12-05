2235b8d776d9525a601c68558439128c3b38f5a6
aaron staes <aaron.staes@gmail.com>
2021-12-05 14:39:45 +0100

    updated image paths

--- src/components/Creator.jsx
+++ src/components/Creator.jsx
@@ -2,7 +2,7 @@ import { StyledCreator } from "./styles/Creator.styled";
 const Creator = () => {
   return (
     <StyledCreator>
-      <img src="../../images/image-avatar.png" alt="" />
+      <img src="./images/image-avatar.png" alt="avatar of person" />
       <p>
         Creation of <span>Jules Wyvern</span>
       </p>
